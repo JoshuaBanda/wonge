f1a5ea7b10d7f705f480ac3707d1f14642b6d8ef
JoshuaBanda <bsc-inf-04-22@unima.ac.mw>
2025-02-11 18:59:40 +0200

    h

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import LandingPage from './LandingPAge'
 import LoginPage from './LoginPage'
 
 export default function Home() {
