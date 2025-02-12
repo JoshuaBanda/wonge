14b0914b67e7ef6efd83b5ad363a2e1e27c72438
JoshuaBanda <bsc-inf-04-22@unima.ac.mw>
2025-02-12 14:40:29 +0200

    toda

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,3 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import LoginPage from './LoginPage'
-import HomePage from './HomePage';
 import LandingPage from './LandingPAge';
 
 export default function Home() {
