261039f3636526301965c6c073b38e486ab909f5
JoshuaBanda <bsc-inf-04-22@unima.ac.mw>
2025-02-12 14:57:04 +0200

    toda

--- pages/_app.js
+++ pages/_app.js
@@ -2,21 +2,23 @@ import { useRouter } from 'next/router';
 import Navbar from '../components/Navbar'; // Adjust path if necessary
 import '../styles/globals.css'; // Import your global styles
 import Footer from '../components/Footer';
+
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
 
-
-  // Check if the user is on the landing page, so we don't show the navbar there
-  const isLandingPage = router.pathname === '/LandingPage'||'/LogingPage';
+  // Correct check: only render Navbar and Footer on the correct pages
+  const isLandingPage = router.pathname === '/' || router.pathname === '/LoginPage' ||router.pathname==='/userAunthentication/SignUpPage'||router.pathname==='/userAunthentication/CreateAccountPage';  // Fixed typo here
 
   return (
     <>
-    {/* Render the navbar only if the user is not on the landing page */}
-    {!isLandingPage && <Navbar />}
+      {/* Render the navbar only if the user is not on the landing page */}
+      {!isLandingPage && <Navbar />}
 
-    {/* Render the page component */}
-    <Component {...pageProps} />
-    {!isLandingPage &&<Footer/>}
+      {/* Render the page component */}
+      <Component {...pageProps} />
+
+      {/* Render Footer only if not on the landing page */}
+      {!isLandingPage && <Footer />}
     </>
   );
 }
